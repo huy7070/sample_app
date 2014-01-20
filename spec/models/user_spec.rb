3637fb65a48d3e8c5a950992010113c424009903
huy7070 <nguy1702@umn.edu>
2014-01-19 20:33:28 -0600

    Test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,22 @@ require 'spec_helper'
 describe User do
 
 #Runs code inside the block. Creates @user instance.
-before {@user = User.new(name: "example User", email: "user@example.com")}
+before {@user = User.new(name: "example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")}
 
 #subject makes @user the default subject of the test example
 subject{@user}
 
+
+
 #Ensures that contructors are valid.
 it {should respond_to(:name)}
 it {should respond_to(:email)}
+#Ensure the User object has a password_digest column
+it {should respond_to(:password_digest)}
+it {should respond_to(:password)}
+it {should respond_to(:password_confirmation)}
+it {should respond_to(:authenticate)}
+
 
 #Sanity check. Verifying the @user is initially valid
 it {should be_valid}
@@ -33,7 +41,58 @@ describe "when name is too long" do
 	it (should_not_be_valid}
 end
 
+#Test email format validation if INVALID
+describe "when email format is invalid" do
+	it "should be invalid do" do
+	addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+	addresses.each do |invalid_address|
+	expect(@user).not_to_be_valid
+	end
+	end
+end
 
+#Test email format validation if VALID
+describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  
+#Test for rejection of duplicate email addresses.  Insensitive to case. 
+ describe "when email address is already taken" do
+	before do
+	user_with_same_email = @user.dup
+	user_with_same_email.save
+	user_with_same_email.email = @user.email.upcase
+	end
+it {should_not_be_valid}
+end
+
+#Validate password presence
+describe "when password is not present" do
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+  end
+  it { should_not be_valid }
+end
+
+#Test case of mismatch
+describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatch" }
+  it { should_not be_valid }
+end
+
+#Tests if password is too short
+describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
 
 
 	
